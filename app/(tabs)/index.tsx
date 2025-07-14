1c8bafe8d55afcfd0e430b31eaec3e7196f71b8f
NguyenLong <long04dz@gmail.com>
2025-07-14 09:04:57 +0700

    day2-usestate

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,21 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, Text } from 'react-native';
+import { Platform, StyleSheet, View, Text, Button } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { useState } from 'react';
 
 export default function HomeScreen() {
+
+  const [count,useCount]= useState<number>(0)
+
   return (
     <View style={styles.reactText}>
-      <Text style={{color:"red"}}>hello word</Text>
+      <Button title='increase' onPress={()=>{useCount(count+1)}}>
+      </Button>
+      <Text style={{color:'red'}}>count : {count}</Text>
     </View>
   );
 }
