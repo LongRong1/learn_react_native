0eff8adae8d1a7f6554b95e1839f5ae7e7fd7629
NguyenLong <long04dz@gmail.com>
2025-07-15 21:04:14 +0700

    day2-input

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, Text, Button } from 'react-native';
+import { Platform, StyleSheet, View, Text, Button, TextInput } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -10,12 +10,21 @@ import { useState } from 'react';
 export default function HomeScreen() {
 
   const [count,useCount]= useState<number>(0)
+  const [name,setName] = useState<string>("")
 
   return (
     <View style={styles.reactText}>
       <Button title='increase' onPress={()=>{useCount(count+1)}}>
       </Button>
       <Text style={{color:'red'}}>count : {count}</Text>
+      <View style={{flexDirection:"row"}}>
+        <Text style={{color:"white"}}>Your name</Text>
+      <TextInput style={styles.input} value={name} onChangeText={setName}></TextInput>
+      
+      </View>
+      <Button title='submit' onPress={()=> setName(name)}>
+      </Button>
+      <Text style={{color:"white"}}>Hello {name}</Text>
     </View>
   );
 }
@@ -34,5 +43,13 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    borderColor: '#888',
+    width:100,
+    color:'white',
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 5,
   },
 });
