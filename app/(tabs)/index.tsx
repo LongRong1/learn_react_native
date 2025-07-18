60f7edcb4feded9ef7cb82484c849a3252c3fccd
NguyenLong <long04dz@gmail.com>
2025-07-18 13:15:00 +0700

    day4-flatlist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,12 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet, View, Text, Button, TextInput } from 'react-native';
+import { Platform, StyleSheet, View, Text, Button, TextInput, FlatList } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useState } from 'react';
+
 
 export default function HomeScreen() {
 
@@ -17,16 +18,34 @@ export default function HomeScreen() {
     {id:3,name:"long2",age:20},
     {id:4,name:"long3",age:21},
     {id:5,name:"long4",age:22},
+    {id:6,name:"long5",age:18},
+    {id:7,name:"long6",age:19},
+    {id:8,name:"long7",age:20},
+    {id:9,name:"long8",age:21},
+    {id:10,name:"long9",age:22},
   ])
 
-  return (
+  return(
     <View style={styles.reactText}>
-      {arr.map((item) => (
-        <View key={item.id} style={{ marginBottom: 10}}>
-          <Text style={{color:"red",backgroundColor:"white"}}>Name: {item.name}</Text>
-          <Text style={{color:"red"}}>Age: {item.age}</Text>
-        </View>
-      ))}
+      <Text style={{color:"white"}}>LongNe</Text>
+    <FlatList
+      data={arr}
+      renderItem={(data)=>{
+        return(
+          <View>
+            <Text
+            style={
+              {
+                color:"red",
+                margin:100,
+                backgroundColor:"white",
+              }
+            }
+            >{data.item.name}</Text>
+          </View>
+        )
+      }}
+    />
     </View>
   );
 }
