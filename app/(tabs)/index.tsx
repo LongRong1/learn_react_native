8d0f4bdb751e8f85699809add182ffd9bdc663e9
NguyenLong <long04dz@gmail.com>
2025-07-19 20:38:30 +0700

    day5-todolist

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -7,62 +7,66 @@ import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
 import { useState } from 'react';
 
+interface itodo{
+  id:number;
+  name:string
+}
 
 export default function HomeScreen() {
 
- 
-  const [name,setName] = useState<string>("")
-  const [arr,setArr] = useState([
-    {id:1,name:"long",age:18},
-    {id:2,name:"long1",age:19},
-    {id:3,name:"long2",age:20},
-    {id:4,name:"long3",age:21},
-    {id:5,name:"long4",age:22},
-    {id:6,name:"long5",age:18},
-    {id:7,name:"long6",age:19},
-    {id:8,name:"long7",age:20},
-    {id:9,name:"long8",age:21},
-    {id:10,name:"long9",age:22},
-  ])
+  const [todo, setTodo] = useState('')
+  const [listtodo, setListTodo] = useState<itodo[]>([])
+
+  const handleClick=()=>{
+    if(!todo) return;
+    setListTodo([...listtodo,{id:1,name:todo}]);
+    setTodo('')
+  }
 
   return(
     <View style={styles.reactText}>
-      <Text style={{color:"white"}}>LongNe</Text>
-    <FlatList
-      data={arr}
-      renderItem={(data)=>{
-        return(
-          <View>
-            <Text
-            style={
-              {
-                color:"red",
-                margin:100,
-                backgroundColor:"white",
-              }
-            }
-            >{data.item.name}</Text>
-          </View>
-        )
-      }}
-    />
+      <Text style={{paddingTop:200}}>Long Ne</Text>
+
+      <View id='input'>
+        <Text>Form</Text>
+        <TextInput style={{color:"red",width:100,borderBottomColor:"black",borderBottomWidth:1,margin:10}}
+          onChangeText={(value)=>setTodo(value)}
+          value={todo}
+        ></TextInput>
+        <Button title='Add' 
+        onPress={handleClick}
+        ></Button>
+      </View>
+
+      <View id='list'>
+        <Text>list</Text>
+        <FlatList
+          data={listtodo}
+          keyExtractor={item => item.id+""}
+          renderItem={({item}) =>{
+            return(
+              <Text style={styles.todoItem}>{item.name}</Text>
+            )
+
+          }}
+        ></FlatList>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
+
   reactText: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
+    backgroundColor:"white",
+  },
+  todoItem:{
+    fontSize:30,
+    borderWidth:1,
+    borderStyle:"solid",
+    marginBottom:10
   },
 });
